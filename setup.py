81247f7b633b70c058824866defa5ec69306c5ff
Yifan Qiao <yifanqiao@ucla.edu>
2025-07-17 14:37:48 -0700

    fix [setup]: fix cxx abi version

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from typing import List
 from setuptools import find_packages, setup
 
 try:
+    import torch
     from torch.utils.cpp_extension import (BuildExtension, CUDAExtension,
                                            include_paths, library_paths)
 except ImportError:
@@ -27,12 +28,23 @@ def get_csrc_files(path) -> List[str]:
 def get_extensions():
     csrc_files = get_csrc_files(CSRC_PATH)
 
+    # Get the C++ ABI flag from PyTorch
+    cxx_abi = torch._C._GLIBCXX_USE_CXX11_ABI
+
+    extra_compile_args = [
+        "-std=c++17", f"-D_GLIBCXX_USE_CXX11_ABI={int(cxx_abi)}"
+    ]
+
     vmm_ops_module = CUDAExtension(
         "kvcached.vmm_ops",
         csrc_files,
         include_dirs=include_paths() + [os.path.join(CSRC_PATH, "inc")],
         library_dirs=library_paths(),
         libraries=["torch", "torch_cpu", "torch_python", "cuda"],
+        extra_compile_args={
+            "cxx": extra_compile_args,
+            "nvcc": extra_compile_args
+        },
     )
     return [vmm_ops_module], {"build_ext": BuildExtension}
 
